f11b1c37f47914a5f0f003cc491782a1686beb40
Biliqis Onikoyi <105673949+BiliqisO@users.noreply.github.com>
2025-01-24 01:53:35 +0100

    git fix

--- shamir-secret-sharing-threshhold-scheme/target/debug/build/libc-57a6c5ac4345ebbe/build_script_build-57a6c5ac4345ebbe.d
+++
@@ -1,5 +0,0 @@
-/Users/mac/Documents/zk-masterclass/zk-implementations/shamir-secret-sharing-threshhold-scheme/target/debug/build/libc-57a6c5ac4345ebbe/build_script_build-57a6c5ac4345ebbe: /Users/mac/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.169/build.rs
-
-/Users/mac/Documents/zk-masterclass/zk-implementations/shamir-secret-sharing-threshhold-scheme/target/debug/build/libc-57a6c5ac4345ebbe/build_script_build-57a6c5ac4345ebbe.d: /Users/mac/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.169/build.rs
-
-/Users/mac/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.169/build.rs:
--- shamir-secret-sharing-threshhold-scheme/target/debug/build/typenum-bfde2d22d6a8851f/build_script_main-bfde2d22d6a8851f.d
+++
@@ -1,7 +0,0 @@
-/Users/mac/Documents/zk-masterclass/zk-implementations/shamir-secret-sharing-threshhold-scheme/target/debug/build/typenum-bfde2d22d6a8851f/build_script_main-bfde2d22d6a8851f: /Users/mac/.cargo/registry/src/index.crates.io-6f17d22bba15001f/typenum-1.17.0/build/main.rs /Users/mac/.cargo/registry/src/index.crates.io-6f17d22bba15001f/typenum-1.17.0/build/op.rs /Users/mac/.cargo/registry/src/index.crates.io-6f17d22bba15001f/typenum-1.17.0/build/tests.rs
-
-/Users/mac/Documents/zk-masterclass/zk-implementations/shamir-secret-sharing-threshhold-scheme/target/debug/build/typenum-bfde2d22d6a8851f/build_script_main-bfde2d22d6a8851f.d: /Users/mac/.cargo/registry/src/index.crates.io-6f17d22bba15001f/typenum-1.17.0/build/main.rs /Users/mac/.cargo/registry/src/index.crates.io-6f17d22bba15001f/typenum-1.17.0/build/op.rs /Users/mac/.cargo/registry/src/index.crates.io-6f17d22bba15001f/typenum-1.17.0/build/tests.rs
-
-/Users/mac/.cargo/registry/src/index.crates.io-6f17d22bba15001f/typenum-1.17.0/build/main.rs:
-/Users/mac/.cargo/registry/src/index.crates.io-6f17d22bba15001f/typenum-1.17.0/build/op.rs:
-/Users/mac/.cargo/registry/src/index.crates.io-6f17d22bba15001f/typenum-1.17.0/build/tests.rs:
