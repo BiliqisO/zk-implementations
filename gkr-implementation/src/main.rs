850cfd5ec3c227f780966015587e86fdb7da20c3
Biliqis Onikoyi <105673949+BiliqisO@users.noreply.github.com>
2025-02-09 23:42:53 +0100

    generate gate indices

--- gkr-implementation/src/main.rs
+++ gkr-implementation/src/main.rs
@@ -1,4 +1,7 @@
+use std::ops::Index;
+
 use ark_ff::PrimeField;
+use evaluation_form_poly::EvaluationFormPolynomial;
 fn main() {
     println!("Hello, world!");
 }
@@ -30,6 +33,90 @@ impl<F: PrimeField> Circuit<F> {
     fn add_layer(&mut self, layer: Layer<F>) {
         self.layers.push(layer);
     }
+    fn add_i(indices: Vec<F>, values: Vec<F>) -> F {
+        let mut res = F::from(0);
+        for i in 0..indices.len() {
+            if indices[i] == F::from(0) {
+                res = res * F::from(1) - values[i]
+            } else if indices[i] == F::from(1) {
+                res = res * values[i];
+            }
+        }
+        res
+    }
+    //indices for this should be gotten from
+    fn init_add_i(indices: Vec<F>) {}
+    fn mul_i(indices: Vec<F>, values: Vec<F>) -> F {
+        let mut res = F::from(0);
+        for i in 0..indices.len() {
+            if indices[i] == F::from(0) {
+                res = res * F::from(1) - values[i]
+            } else if indices[i] == F::from(1) {
+                res = res * values[i];
+            }
+        }
+        res
+    }
+    fn w_i(values: Vec<F>) -> EvaluationFormPolynomial<F> {
+        let poly = EvaluationFormPolynomial::new(&values);
+        poly
+    }
+
+    fn generate_gate_indices_for_layer_i(self, layer: usize) -> Vec<Vec<String>> {
+        let layers: Vec<Layer<F>> = self.layers.iter().rev().cloned().collect();
+
+        let outputs: Vec<F> = layers[layer].gates.iter().map(|gate| gate.output).collect();
+        let output_binary: Vec<String> = (0..outputs.len())
+            .map(|i| format!("{:0width$b}", i, width = layer))
+            .collect();
+        println!("output_binary{:?}", output_binary);
+
+        let left_right: Vec<F> = layers[layer]
+            .gates
+            .iter()
+            .flat_map(|gate| vec![gate.left, gate.right])
+            .collect();
+        let left_right_binary: Vec<String> = (0..left_right.len())
+            .map(|i| format!("{:0width$b}", i, width = layer + 1))
+            .collect();
+        println!("left_right_binary{:?}", left_right_binary);
+        let gate_indices: Vec<Vec<String>> = output_binary
+            .iter()
+            .enumerate()
+            .map(|(i, output)| {
+                vec![
+                    output.clone(),
+                    left_right_binary[i * 2].clone(),
+                    left_right_binary[i * 2 + 1].clone(),
+                ]
+            })
+            .collect();
+        println!("gate_indices{:?}", gate_indices);
+        gate_indices
+    }
+    //values is w for that layer
+    fn generate_fbc(self, i: usize, op: Op, b: F, c: F, values: Vec<F>) {
+        // let layer_op = self.layers
+        // let layer_indices =
+        // This should go out of this fuction
+
+        for gate in &self.layers[i].gates {
+            match gate.op {
+                Op::Add => {
+
+                    // let add_sum: F = Circuit::add_i(indices, values) ;
+                    // let fbc =
+
+                    //this is valid but not in the right place
+                    // let w0  = Circuit::w_i(values.clone()).partial_evaluate(b, 0).representation;
+                    // let w1 = Circuit::w_i(values).partial_evaluate(c, 0).representation;
+                    // let w:Vec<F>  =     w0.iter().zip(w1.iter()).map(|(a, b)| *a + *b).collect();
+                }
+
+                Op::Mul => todo!(),
+            }
+        }
+    }
 }
 impl<F: PrimeField> Layer<F> {
     fn new() -> Self {
@@ -45,7 +132,6 @@ impl<F: PrimeField> Layer<F> {
         println!("output{:?}", output);
         self.gates.drain(..);
         for i in 0..ops.len() {
-
             let new_gate = Gate::new(output[i], output[i + 1], ops[i]);
 
             self.gates.push(new_gate);
@@ -66,11 +152,6 @@ impl<F: PrimeField> Gate<F> {
             op,
         }
     }
-
-    fn layer(self, mut gates: Vec<Gate<F>>) -> Vec<Gate<F>> {
-        gates.push(self);
-        gates
-    }
 }
 
 #[cfg(test)]
@@ -87,7 +168,7 @@ mod tests {
     }
 
     #[test]
-    fn test_evaluate_layers() {
+    fn test_compute_circuits() {
         let left = Fq::from(1u64);
         let right = Fq::from(2u64);
         let gate1 = Gate::new(left, right, Op::Add);
@@ -119,5 +200,15 @@ mod tests {
         circuit.add_layer((layer.clone()));
 
         println!(" circuit{:?}", circuit);
+        let v = circuit.layers[0].clone();
+        println!("v {:?}", v);
+        circuit.generate_gate_indices_for_layer_i(1);
+    }
+    #[test]
+    fn test_addi() {
+        let indices = vec![Fq::from(0), Fq::from(0), Fq::from(1)];
+        let values = vec![Fq::from(4), Fq::from(2), Fq::from(3)];
+        let res = Circuit::add_i(indices, values);
+        println!("res {:?}", res)
     }
 }
