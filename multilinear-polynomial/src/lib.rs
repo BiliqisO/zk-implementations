6f0aa9e65191b0068f57951b700a2320eccb852c
Biliqis Onikoyi <105673949+BiliqisO@users.noreply.github.com>
2025-02-20 18:21:15 +0100

    eval poly add pub

--- multilinear-polynomial/src/lib.rs
+++ multilinear-polynomial/src/lib.rs
@@ -13,7 +13,7 @@ pub struct EvaluationFormPolynomial<F: PrimeField> {
 impl<F: PrimeField> Add for EvaluationFormPolynomial<F> {
     type Output = Self;
 
-    fn add(self, other: Self) -> Self {
+    pub fn add(self, other: Self) -> Self {
         let mut result = self.clone();
         for (i, coeff) in other.representation.iter().enumerate() {
             result.representation[i] += coeff;
