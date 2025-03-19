350a58a938bae1b046d0611737856d1a13dc0e7b
Biliqis Onikoyi <105673949+BiliqisO@users.noreply.github.com>
2025-03-19 18:39:23 +0100

    trusted setup done

---
+++ ml-kzg/src/main.rs
@@ -0,0 +1,59 @@
+use ark_bn254::g2;
+use ark_ff::PrimeField;
+use ark_ec::pairing::{self, Pairing};
+use ark_ec::PrimeGroup;
+use multilinear_polynomial::{boolean_hypercube::boolean_hypercube};
+use std::marker::PhantomData;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+struct Tau<P: Pairing> {  
+    lagrange_basis: Vec<P::G1>,
+    g2_tau:Vec<P::G2>,  
+
+}
+impl<P:Pairing> Tau<P> {
+    fn initialise<F: PrimeField>(taus:Vec<F>)->Self{
+        let generator1 =P::G1::generator();
+        let generator2 =P::G2::generator();
+
+        
+        let no_of_variables = taus.len();
+        let hypercube = boolean_hypercube::<F>(no_of_variables).into_iter().collect::<Vec<_>>();
+        let mut lagrange_basis = Vec::new();
+        for point in &hypercube {
+            let mut value = F::one();
+            for (i, bit) in point.chars().enumerate() {
+            if bit == '1' {
+                value *= taus[i];
+            } else {
+                value *= F::one() - taus[i];
+            }
+            }
+            lagrange_basis.push(generator1.mul_bigint(value.into_bigint()));
+        }
+        let mut g2_tau = Vec::new();
+        g2_tau = taus.iter().map(|i| generator2.mul_bigint(i.into_bigint())).collect::<Vec<_>>();
+     
+        println!("lagrange_basis {:?}", lagrange_basis);
+        println!("g2_tau {:?}", g2_tau);
+        Tau { lagrange_basis, g2_tau}
+       
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ark_bn254::Fq;
+    #[test]
+        fn test_initialise(){  
+        let taus = vec![Fq::from(45u64), Fq::from(2u64), Fq::from(3u64)];
+        Tau::< ark_bn254::Bn254>::initialise(taus);        
+
+        }
+ 
+
+
+}
